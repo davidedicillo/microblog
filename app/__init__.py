c51d0b40c4a1c9e224384ae10993d795d5259d90
Davide Di Cillo <davide@39inc.com>
2020-10-11 13:54:53 +0000

    Getting ready to deply to Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
@@ -31,13 +33,20 @@ if not app.debug:
 			credentials=auth, secure=secure)
 		mail_handler.setLevel(logging.ERROR)
 		app.logger.addHandler(mail_handler)
-	if not os.path.exists('logs'):
+	if app.config['LOG_TO_STDOUT']:
+		stream_handler = logging.StreamHandler()
+		stream_handler.setLevel(logging.INFO)
+		app.logger.addHandler(stream_handler)
+	else:
+		if not os.path.exists('logs'):
 			os.mkdir('logs')
-			file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-			file_handler.setFormatter(logging.Formatter(
-				'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-			file_handler.setLevel(logging.INFO)
-			app.logger.addHandler(file_handler)
-		
-			app.logger.setLevel(logging.INFO)
-			app.logger.info('Microblog startup')
+		file_handler = RotatingFileHandler('logs/microblog.log',
+										   maxBytes=10240, backupCount=10)
+		file_handler.setFormatter(logging.Formatter(
+			'%(asctime)s %(levelname)s: %(message)s '
+			'[in %(pathname)s:%(lineno)d]'))
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Microblog startup')
--- config.py
+++ config.py
@@ -14,4 +14,7 @@ class Config(object):
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['davide@dicillo.com']
 	
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+	
 	POSTS_PER_PAGE = 25
